b67d1296ae10c650f79e357af4b02d726afc427f
dueka <ekanemd@live.com>
2020-01-23 14:46:56 +0100

    created the recipe and ingredients

--- data/seeds/01-ingredients.js
+++ data/seeds/01-ingredients.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("ingredients")
+  return knex("IngredientsUpdate")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("ingredients").insert([
+      return knex("IngredientsUpdate").insert([
         { Ingredient_name: "Afang Leaf", Ingredient_id: 1 },
         { Ingredient_name: "Bread", Ingredient_id: 2 },
         { Ingredient_name: "Carrot", Ingredient_id: 3 }
--- data/seeds/02-recipes.js
+++ data/seeds/02-recipes.js
@@ -1,10 +1,10 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("recipes")
+  return knex("RecipesUpdate")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("recipes").insert([
+      return knex("RecipesUpdate").insert([
         { id: 1, Name: "Sandwich", Ingredient_id: 1, Quantity: 2 },
         { id: 2, Name: "Cole slaw", Ingredient_id: 2, Quantity: 10 },
         { id: 3, Name: "Afang", Ingredient_id: 3, Quantity: 3 }
